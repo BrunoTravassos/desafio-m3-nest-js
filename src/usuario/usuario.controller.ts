85ce233725362c3ac44a285b36097ee1ec7e5ca3
BrunoTravassos <brunodefrontin@gmail.com>
2024-11-12 10:29:41 -0300

    config usuario e create

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+
+@Controller('usuario')
+export class UsuarioController {
+  constructor(private readonly usuarioService: UsuarioService) {}
+
+  @Post()
+  async create(@Body() createUsuarioDto: CreateUsuarioDto) {
+
+    const emailExiste = await this.usuarioService.findEmail(createUsuarioDto.email);
+
+    if(emailExiste){
+      throw new BadRequestException('Email já Cadastrado!')
+    }
+
+    return this.usuarioService.create(createUsuarioDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usuarioService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usuarioService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+    return this.usuarioService.update(+id, updateUsuarioDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usuarioService.remove(+id);
+  }
+}
---
+++ src/usuario/usuario.service.ts
@@ -0,0 +1,75 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from "bcrypt";
+import {v4 as uuidv4} from 'uuid';
+
+@Injectable()
+export class UsuarioService {
+  constructor(private readonly prisma: PrismaService){}
+  
+  async validarCampos(usuario:CreateUsuarioDto){
+    if (!usuario.nome || !usuario.email || !usuario.senha) {
+      return null
+    }
+    return usuario
+  }
+
+  async create(createUsuarioDto: CreateUsuarioDto) {
+
+    const verificaCampos = await this.validarCampos(createUsuarioDto);
+
+    if(verificaCampos===null){
+      throw new BadRequestException(
+        'Todos os campos devem ser obrigatorios'
+      );
+    }
+    try {
+      const senhaCriptografada = await bcrypt.hash(createUsuarioDto.senha,10);
+
+      return await this.prisma.usuarios.create({
+        data:{
+          id: uuidv4(),
+          nome: createUsuarioDto.nome,
+          email: createUsuarioDto.email,
+          senha: senhaCriptografada,
+        },
+      });
+      
+    } catch (error) {
+      const erro = error as Error;
+      throw new BadRequestException({
+        message: erro.message,
+      });
+    }
+  }
+
+  findAll() {
+    return `This action returns all usuario`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} usuario`;
+  }
+
+  async findEmail(email:string){
+
+    const usuario = await this.prisma.usuarios.findUnique({
+      where:{
+        email
+      },
+    });
+
+    return usuario
+
+  }
+
+  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
+    return `This action updates a #${id} usuario`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} usuario`;
+  }
+}
